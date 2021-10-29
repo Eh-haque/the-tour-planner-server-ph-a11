0e5e56a0f2d5a3c5f0a41b76b85aced4b5569f22
Eh-haque <ehtisamul@gmail.com>
2021-10-29 18:07:58 +0600

    ignore

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, Collection } = require('mongodb');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 const cors = require('cors');
 const req = require('express/lib/request');
@@ -13,15 +13,15 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ryljg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function run() {
     try {
         await client.connect();
-        console.log('server connected');
+        // console.log('server connected');
         const database = client.db('assignment_11');
         const tourCollection = database.collection('tour_plan');
+        const orderCollection = database.collection('place_plan');
 
         // add plans
         app.post('/add_plan', async (req, res) => {
@@ -32,6 +32,21 @@ async function run() {
         // get plans
         app.get('/add_plan', async (req, res) => {
             const result = await tourCollection.find({}).toArray();
+            res.send(result)
+        })
+
+        /* // get a plan
+        app.post('/add_plan/:id'), async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            console.log(id);
+            const result = await tourCollection.findOne(query);
+            res.send(result);
+        }
+ */
+        // place order
+        app.post('/add_to_cart', async (req, res) => {
+            const result = await orderCollection.insertOne(req.body);
             res.send(result)
         })
 
