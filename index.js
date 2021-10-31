f75302f4f2f3f33c11267c04031b363cae401242
Eh-haque <ehtisamul@gmail.com>
2021-10-31 16:54:02 +0600

    may done fg

--- index.js
+++ index.js
@@ -35,6 +35,14 @@ async function run() {
             res.send(result);
         })
 
+        // get a plan
+        app.get('/my_orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await myOrderCollection.findOne(query);
+            res.send(result);
+        })
+
         // post plans
         app.post('/add_plan', async (req, res) => {
             const result = await tourCollection.insertOne(req.body);
